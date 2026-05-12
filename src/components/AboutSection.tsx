d7f02d30974d4ee9a0af38c2e2e28f7a8ca5ad5b
Elliot Brazier <justashotoffire@gmail.com>
2026-05-12 16:22:47 -0700

    filled out About section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -5,15 +5,33 @@ export default function AboutSection() {
         <p className="eyebrow">About Me</p>
         <h2>Why software?</h2>
         <p>
-          I got into software because it blends creativity, systems thinking, and real
-          impact. My favorite work sits at the edge of design and engineering where motion,
-          usability, and storytelling all matter.
+            When I was 14 years old I was really addicted to RuneScape, so much so that I wanted to make my own version of it.
+            This lead to me downloading bootleg source code for the game, modifying the code in the Eclipse IDE, 
+            and deploying it for my friends to play before I'd graduated highschool. 
+        <br/>
+            As a career path I love software development because there's always a problem to solve. Approaching business solutions in my own way and 
+            learning from my more experienced peers is something I'll never get tired of. 
         </p>
-      </div>
-      <div>
+        <br/>
+        <h2>What software?</h2>
         <p>
-          I enjoy turning abstract ideas into products people can feel immediately,
-          especially experiences that invite exploration rather than static scrolling.
+            In my free time I experiment with a lot of game engines. I've completed and published a game in the Godot engine, and spent 
+            hundreds of hours in Unreal Engine, though I've never 'finished' a project in it. I've taken online courses offered through Udemy on 
+            procedural animation in UE5, and a general development course in C# for Unity. Game development will always be a passion of mine.
+        <br/>
+            As a developer I've worked with an array of frameworks, apps, and languages. All of the noteworthy ones can be found
+            on my resume, but I am confident in my ability to pick up any language or framework and use it to its fullest potential. 
+        </p>
+        <br/>
+        <h2>Where software?</h2>
+        <p>
+            I grew up in rural Minnesota, and got my degree in Computer Science in Fargo North Dakota at NDSU. After graduating in 2020 I moved to Hillsboro Oregon, where I 
+            now live with my wife and our pets. I've continued feeding my passion as a developer in the Silicon Forest, and plan on keeping it that way!
+        </p>
+        <br/> 
+        <h2>Who software?</h2>
+        <p>
+            I'm not sure I can keep this joke going. Check out work experience section below.
         </p>
       </div>
     </section>
